24352a9eb18e7b58f8e4e2b7cbf2c39aaa3fd63e
吴迪 <dimmortal@wudideMacBook-Pro.local>
2016-10-18 13:58:28 +0800

    Initial Commit

---
+++ HELLO WORLD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HELLO WORLD
+//
+//  Created by 吴迪 on 2016/10/18.
+//  Copyright © 2016年 dimmortal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HELLO WORLD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HELLO WORLD
+//
+//  Created by 吴迪 on 2016/10/18.
+//  Copyright © 2016年 dimmortal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
